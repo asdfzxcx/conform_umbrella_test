35f7c0519ed248c8465bcfc5c9c1f06f47ddafdd
tenkai9 <tenkai.nine@gmail.com>
2017-07-31 13:55:36 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ConformUmbrellaTest
+
+**TODO: Add description**
+
---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule ConformUmbrellaTest.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      apps_path: "apps",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Dependencies listed here are available only for this
+  # project and cannot be accessed from applications inside
+  # the apps folder.
+  #
+  # Run "mix help deps" for examples and options.
+  defp deps do
+    [
+      {:conform, "2.5.0"},
+      {:distillery, "1.4.1"}
+    ]
+  end
+end
